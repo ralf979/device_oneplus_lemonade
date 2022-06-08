9cbdef7b1f391e4ee875e4ddf589bd126174630c
chandu078 <chandudyavanapelli03@gmail.com>
2022-06-08 05:55:41 +0000

    lemonade: Set default hotspot ssid by RRO

--- device.mk
+++ device.mk
@@ -54,7 +54,8 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
-    OPlusSystemUIResTarget
+    OPlusSystemUIResTarget \
+    OPlus9WifiOverlay
 
 # PowerShare
 PRODUCT_PACKAGES += \
